f6140a4afaa8432fcf2e367a392a05c2a06afa7f
kimhyemin9988 <110611596+kimhyemin9988@users.noreply.github.com>
2022-12-09 18:12:40 +0900

    Create jsStudy.js

---
+++ js/jsStudy.js
@@ -0,0 +1,15 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    sub: function(a, b){
+        console.log(a -b);
+    },
+    multiply: function(a, b){
+        console.log(a ** b)
+    },
+};
+
+calculator.add(5, 4);
+calculator.sub(7, 4);
+calculator.multiply(8, 4);
